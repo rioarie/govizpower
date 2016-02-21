88c2269dfd583f8ceb5c65d8896d212f04508323
Febriansyah Nuur Rasyiid <febrian@angon.co.id>
2016-02-21 18:12:59 +0700

    Add Detail Endpoint And Request Mapper

--- controllers/ApiController.go
+++ controllers/ApiController.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	_ "govizpower/datamappers"
 	"govizpower/models"
 	"net/http"
 )
@@ -12,14 +11,34 @@ type ApiController struct{}
 func (a *ApiController) Endpoint_kota(c *gin.Context) {
 	model := models.ApiModel{}
 
-	data := model.GetDataKota()
+	tahun := c.Param("tahun")
+	bulan := c.Param("bulan")
 
-	// if data == (datamappers.KotaMapper{}) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"status":  "0031",
-	// 		"message": "Someting Wrong",
-	// 	})
-	// } else {
-	c.JSON(http.StatusOK, data)
-	// }
+	data := model.GetDataKota(tahun + bulan)
+
+	if data == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "0031",
+			"message": "Something Wrong",
+		})
+	} else {
+		c.JSON(http.StatusOK, data)
+	}
+}
+
+func (a *ApiController) Endpoint_detail(c *gin.Context) {
+	model := models.ApiModel{}
+	tahun := c.Param("tahun")
+	bulan := c.Param("bulan")
+	idupj := c.Param("id")
+
+	data := model.GetDetailKota(idupj, tahun+bulan)
+	if data == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "0031",
+			"message": "Something Wrong",
+		})
+	} else {
+		c.JSON(http.StatusOK, data)
+	}
 }
