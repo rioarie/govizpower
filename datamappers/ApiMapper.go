01e74184331452b147ddfc9b454dd91e262455f8
Febriansyah Nuur Rasyiid <febrian@angon.co.id>
2016-02-21 13:56:24 +0700

    Add KotaContainer

--- datamappers/ApiMapper.go
+++ datamappers/ApiMapper.go
@@ -1,9 +1,9 @@
 package datamappers
 
 type KotaMapper struct {
-	Kota      string
-	Gps_L     string
-	Gps_B     string
+	Kota      string  `db:"NAMAUPJ"`
+	Gps_L     float64 `db:"GPS_L"`
+	Gps_B     float64 `db:"GPS_B"`
 	Kwh_kvarh float64
 	Kwh_lwbp  float64
 	Kwh_wbp   float64
@@ -12,3 +12,5 @@ type KotaMapper struct {
 type ApiMapper struct {
 	Data []KotaMapper
 }
+
+type KotaContainer []KotaMapper
