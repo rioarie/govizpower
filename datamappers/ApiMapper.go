aecd49e93e71df298b4aeef641e77b564234cabc
Febriansyah Nuur Rasyiid <febrian@angon.co.id>
2016-02-21 17:25:47 +0700

    Add mapper for detail

--- datamappers/ApiMapper.go
+++ datamappers/ApiMapper.go
@@ -1,16 +1,23 @@
 package datamappers
 
 type KotaMapper struct {
-	Kota      string  `db:"NAMAUPJ"`
-	Gps_L     float64 `db:"GPS_L"`
-	Gps_B     float64 `db:"GPS_B"`
-	Kwh_kvarh float64
-	Kwh_lwbp  float64
-	Kwh_wbp   float64
+	Id        int     `db:"ID_UPJ" json:"id"`
+	Kota      string  `db:"NAMAUPJ" json:"kota"`
+	Gps_L     float64 `db:"GPS_L" json:"gps_l"`
+	Gps_B     float64 `db:"GPS_B" json:"gps_b"`
+	Kwh_kvarh float64 `json:"kwh_kvarh"`
+	Kwh_lwbp  float64 `json:"kwh_lwbp"`
+	Kwh_wbp   float64 `json:"kwh_wbp"`
 }
 
-type ApiMapper struct {
-	Data []KotaMapper
+type DetailMapper struct {
+	Id        int     `db:"IDPELANGGAN" json:"id"`
+	Gps_L     float64 `db:"GPS_L" json:"gps_l"`
+	Gps_B     float64 `db:"GPS_B" json:"gps_b"`
+	Kwh_kvarh float64 `json:"kwh_kvarh"`
+	Kwh_lwbp  float64 `json:"kwh_lwbp"`
+	Kwh_wbp   float64 `json:"kwh_wbp"`
 }
 
 type KotaContainer []KotaMapper
+type DetailContainer []DetailMapper
